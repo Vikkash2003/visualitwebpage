5745520b7d91797f5adfeb424c10feb077a34478
Raqeeb Rameez <103761347+Walapalam@users.noreply.github.com>
2025-10-08 16:09:20 +0530

    Contains the updated privacy policy screen

--- visualit/src/app/joinUsPage/profileCreation/createAccountPage/page.js
+++
@@ -1,7 +0,0 @@
-// visualit/src/app/joinUsPage/profileCreation/page.js
-'use client'
-import CreateAccountPage from '../createAccountPage';
-
-export default function Page() {
-    return <CreateAccountPage />;
-}
